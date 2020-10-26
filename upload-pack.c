f0f625cb3ce556207d22c4446d4024b8eebc31a0
Josh Steadmon <steadmon@google.com>
2020-10-26 14:40:44 -0700

    upload-pack, serve: log received client trace2 SID
    When upload-pack (protocol v0/v1) or a protocol v2 server receives a
    trace2-sid capability from a client, log the received session ID via a
    trace2 data event.
    
    Change-Id: Ie002891e56fe6619b03892da5bb472d1192427b8

--- upload-pack.c
+++ upload-pack.c
@@ -1059,6 +1059,7 @@ static void receive_needs(struct upload_pack_data *data,
 		const char *features;
 		struct object_id oid_buf;
 		const char *arg;
+		int feature_len;
 
 		reset_timeout(data->timeout);
 		if (packet_reader_read(reader) != PACKET_READ_NORMAL)
@@ -1110,6 +1111,12 @@ static void receive_needs(struct upload_pack_data *data,
 		if (data->allow_filter &&
 		    parse_feature_request(features, "filter"))
 			data->filter_capability_requested = 1;
+		if ((arg = parse_feature_value(features, "trace2-sid", &feature_len, 0)))
+		{
+			char *client_sid = xstrndup(arg, feature_len);
+			trace2_data_string("trace2", NULL, "client-sid", client_sid);
+			free(client_sid);
+		}
 
 		o = parse_object(the_repository, &oid_buf);
 		if (!o) {
