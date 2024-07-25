18513ded1755ef795a9bd2ed844c26fa86fed255
Calvin Wan <calvinwan@google.com>
2024-07-25 21:21:17 +0000

    contrib/cgit-rs: add repo initialization and config access
    Co-authored-by: Calvin Wan <calvinwan@google.com>
    Signed-off-by: Calvin Wan <calvinwan@google.com>
    Change-Id: I6dd886af8c63e1f0f3251064cd8903aecdf768bb

--- contrib/cgit-rs/cgit-sys/public_symbol_export.c
+++ contrib/cgit-rs/cgit-sys/public_symbol_export.c
@@ -2,10 +2,34 @@
 // original symbols can be hidden. Renaming these with a "libgit_" prefix also
 // avoid conflicts with other libraries such as libgit2.
 
+#include "git-compat-util.h"
 #include "contrib/cgit-rs/cgit-sys/public_symbol_export.h"
+#include "common-init.h"
+#include "config.h"
+#include "setup.h"
 #include "version.h"
 
 #pragma GCC visibility push(default)
+
+const char *libgit_setup_git_directory(void)
+{
+	return setup_git_directory();
+}
+
+int libgit_config_get_int(const char *key, int *dest)
+{
+	return git_config_get_int(key, dest);
+}
+
+void libgit_init_git(const char **argv)
+{
+	init_git(argv);
+}
+
+int libgit_parse_maybe_bool(const char *val)
+{
+	return git_parse_maybe_bool(val);
+}
 
 const char *libgit_user_agent(void)
 {
